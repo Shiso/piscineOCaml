c27f3c48f1351c22837834a17044eed9b0b1a41b
Theo DENISET <tdeniset@e2r5p22.42.fr>
2015-06-20 23:04:47 +0200

    rush00 v0.01

--- rush00/main.ml
+++ rush00/main.ml
@@ -1,50 +1,53 @@
 
-type case = Nil
-	| J1
-	| J2
-	| Board of case list
+type value = Nil
+	| P1
+	| P2
 
+type case = {
+	chip: value;
+	inc:  case list
+}
 
 
 let toString = function
 	| Nil -> "-"
-	| J1  -> "O"
-	| J2  -> "X"
-	| _ -> "Yolo"
-
+	| P1  -> "O"
+	| P2  -> "X"
 
 let getCase board x y =
 	let bigN = (((x - 1) / 3) * 3) + ((y - 1) / 3) in
 	let smallN = (((x - 1) mod 3) * 3) + ((y - 1) mod 3) in
 	let rec loop n = function
-		| [] -> Nil
+		| [] -> {chip=Nil; inc=[]}
 		| h::t -> if n = 0 then h
 				  else loop (n - 1) t
 	in
-	let tmp = loop bigN board in
-	match tmp with
-		| Board -> loop smallN (h::t)
-		| _ -> tmp
+	let tmp = loop bigN board.inc in
+	let {chip=n; inc=l} = tmp in
+	if l = [] then n
+	else let ret = loop smallN l in ret.chip
 
-let init_board () : case list =
-	let board = [] in
+let init_board () : case =
+	let board = {chip=Nil; inc=[]} in
 	let rec fill_nil acc = function
 		| 0 -> acc
-		| n -> fill_nil (acc @ [Nil]) (n - 1)
+		| n when n = 3 -> fill_nil (acc @ [{chip=P1; inc=[]}]) (n - 1)
+		| n when n = 5 -> fill_nil (acc @ [{chip=P2; inc=[]}]) (n - 1)
+		| n -> fill_nil (acc @ [{chip=Nil; inc=[]}]) (n - 1)
 	in
 	let rec loop acc nb =
 		if (nb = 0)
 			then acc
 		else
-			loop (acc @ (fill_nil [] 9)) (nb - 1)
+			loop (acc @ [{chip=Nil; inc=(fill_nil [] 9)}]) (nb - 1)
 	in
-	loop board 9
+	let ret = {chip=Nil; inc=(loop board.inc 9)} in ret
 
 let print_board board =
 	let rec loop c = function
 		| 81 -> ()
 		| nb -> 
-			let c = getCase board (nb mod 9) (nb / 9) in
+			let c = toString @@ getCase board (nb mod 9) (nb / 9) in
 			if (((nb + 1) mod 9) = 0) then print_string (c^"\n")
 			else if (((nb + 1) mod 3) = 0) then print_string (c^" | ")
 			else print_string (c^" ") ;

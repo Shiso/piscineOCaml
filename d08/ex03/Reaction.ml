2053390a95bcdcfb672df727b04afb2b61f9f76e
Theo DENISET <tdeniset@e2r5p16.42.fr>
2015-06-26 19:16:50 +0200

    d08ex03 remove useless comment

--- d08/ex03/Reaction.ml
+++ d08/ex03/Reaction.ml
@@ -7,25 +7,6 @@ class virtual reaction (inlst:(Molecule.molecule * int) list) (outlst:(Molecule.
 		method virtual get_result : (Molecule.molecule * int) list
 
 		method virtual balance : reaction
-		method virtual is_balanced : bool (* =
-			let rec get_atoms_list res (lst:(Molecule.molecule * int) list) = match lst with
-				| [] -> res
-				| h::t -> let rec loop res = (function
-								| 0 -> res
-								| x -> loop (res @ ((fst h)#get_atoms)) (x - 1))
-							in
-							get_atoms_list (loop res (snd h)) t
-			in
-			let inAtoms = get_atoms_list [] _start
-			and outAtoms = get_atoms_list [] _result in
-			let sortedIn = List.sort (fun h t -> (compare (h#get_symbol) (t#get_symbol))) inAtoms
-			and sortedOut = List.sort (fun h t -> (compare (h#get_symbol) (t#get_symbol))) outAtoms in
-			let rec compare (inl : Atom.atom list) (out : Atom.atom list) = match inl, out with
-				| [], [] -> true
-				| _ , []-> false
-				| [], _ -> false
-				| h1::t1, h2::t2 -> if (h1#get_symbol) = (h2#get_symbol) then compare t1 t2 else false
-			in
-			compare sortedIn sortedOut *)
+		method virtual is_balanced : bool
 
 	end
